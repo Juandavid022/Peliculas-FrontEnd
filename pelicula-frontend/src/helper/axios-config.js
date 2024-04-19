a103aefad1c32759810fdf79bcf7612217d81853
Juandavid022 <58521859+Juandavid022@users.noreply.github.com>
2024-04-19 08:41:34 -0500

    cambios de url base

--- pelicula-frontend/src/helper/axios-config.js
+++ pelicula-frontend/src/helper/axios-config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({baseURL:'http://localhost:4000/'});
+const axiosInstance = axios.create({baseURL:'https://ingweb2.onrender.com/'});
 
 export {axiosInstance,}
